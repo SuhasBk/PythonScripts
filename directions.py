78e9c0bb4966d26a7d4b55485de3a06a8d367717
Suhas Bk <kowligi1998@gmail.com>
2020-03-03 18:54:58 +0530

    choice between arguments or input

--- directions.py
+++ directions.py
@@ -3,8 +3,10 @@ import webbrowser
 import sys
 
 if len(sys.argv[1:]) != 2:
-    exit("Usage : directions.py [src] [dest]")
+    src = input("Enter the source...\n> ")
+    dest = input("Enter the destination...\n> ")
 else:
     src = sys.argv[1]
     dest = sys.argv[2]
-    webbrowser.open("https://maps.google.com/maps?saddr={}&daddr={}".format(src,dest))
+
+webbrowser.open("https://maps.google.com/maps?saddr={}&daddr={}".format(src,dest))
