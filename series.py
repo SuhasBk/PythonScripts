c1683474804b8a5d3235b0a34be533076e708522
Suhas BK <kowligi1998@gmail.com>
2020-05-29 23:01:21 +0530

    code refactoring

--- series.py
+++ series.py
@@ -1,32 +1,46 @@
 #!/usr/bin/python3
-from subprocess import *
+from subprocess import Popen,PIPE
 import os,sys
 
 if sys.platform=='linux':
-    DIR = '/mnt/Universe/tv_movies/series'
+    ROOT_DIR = '/mnt/Universe/tv_movies/series'
 else:
-    DIR = r'D:\tv_movies\series'
-
-series_list = os.listdir(DIR)
-for i,j in enumerate(series_list[:]):
-    print(i,' : ',j)
-sh = int(input("Enter the choice:\n> "))
+    ROOT_DIR = r'E:\tv_movies\series'
+    
+series_list = list(map( lambda x : os.path.join(ROOT_DIR,x),os.listdir(ROOT_DIR)))
+series_list = list(filter(lambda x : os.path.isdir(x),series_list))
 
 for i,j in enumerate(series_list):
-    if sh==i:
-        season_list = os.listdir(os.path.join(DIR,j))
-        for p,q in enumerate(season_list[:]):
-            print(p,' : ',q)
-        se = int(input("Enter the choice:\n> "))
+    print(i,' : ',j[j.rfind(os.path.sep)+1:])
 
-        for k,l in enumerate(season_list[:]):
-            if se==k:
-                f = os.path.join(DIR,j,l)
+ch = int(input("Enter the choice:\n> "))
 
-                if os.path.isdir(f):
-                    episode_list = list(filter(lambda x : '.mkv' in x or '.mp4' in x or '.avi' in x,os.listdir(os.path.join(DIR,j,l))))
-                    e = os.path.join(DIR,j,l,episode_list[0])
-                    Popen(["vlc",e],stdout=PIPE,stderr=PIPE)
+series = os.path.join(ROOT_DIR, series_list[ch])
 
-                elif os.path.isfile(f):
-                    Popen(["vlc",f],stdout=PIPE,stderr=PIPE)
+season_list = os.listdir(series)
+for p,q in enumerate(season_list):
+    print(p,' : ',q)
+
+ch = int(input("Enter the choice:\n> "))
+
+season = os.path.join(series,season_list[ch])
+
+if os.path.isdir(season):
+    episode_list = os.listdir(season)
+    episode_list = list(filter(lambda x : '.srt' not in x and '.txt' not in x,episode_list))
+    
+    for z,w in enumerate(episode_list):
+        print(z,' : ',w)
+    
+    ch = int(input("Enter the choice:\n> "))
+
+    episode = os.path.join(season,episode_list[ch])
+    
+    if os.path.isdir(episode):
+        fname = list ( filter ( lambda x : '.srt' not in x and '.txt' not in x, os.listdir(episode) ) )
+        Popen(["vlc",fname[0]],stdout=PIPE,stderr=PIPE)
+    else:
+        Popen(["vlc",episode],stdout=PIPE,stderr=PIPE)
+
+elif os.path.isfile(season):
+    Popen(["vlc",season],stdout=PIPE,stderr=PIPE)
