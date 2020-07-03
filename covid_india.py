debf76465fcd0aadbbc7f5b288e0ee406c737a85
Suhas Bk <kowligi1998@gmail.com>
2020-07-03 12:19:15 +0530

    UX updates

--- covid_india.py
+++ covid_india.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import requests
+import sys
 from threading import Thread
+
+def abort(msg):
+    sys.exit(msg)
 
 def fetch():
     global data1, data2
@@ -10,21 +14,14 @@ def fetch():
     data1 = api1.json()
     data2 = api2.json()
 
-def print_paramters(data,type):
-    if type=='district':
-        active = data['active']
-        confirmed = data['confirmed']
-        recovered = data['recovered']
-        deceased = data['deceased']
+def print_paramters(data):
+    active = data.get('active') if data.get('active') else 'NA'
+    confirmed = data.get('confirmed') if data.get('confirmed') else 'NA'
+    recovered = data.get('recovered') if data.get('recovered') else 'NA'
+    deceased = data.get('deceased') if data.get('deceased') else 'NA'
+    tested = data.get('tested') if data.get('tested') else 'NA'
 
-        print(f"\nACTIVE cases : {active}\nCONFIRMED cases : {confirmed}\nRECOVERED cases : {recovered}\nDECEASED cases : {deceased}\n")
-    else:
-        confirmed = data['confirmed']
-        deceased = data['deceased']
-        recovered = data['recovered']
-        tested = data['tested']
-
-        print(f"\nCONFIRMED cases : {confirmed}\nRECOVERED cases : {recovered}\nDECEASED cases : {deceased}\nTESTED : {tested}\n")
+    print(f"\nACTIVE cases : {active}\nTESTED cases : {tested}\nCONFIRMED cases : {confirmed}\nRECOVERED cases : {recovered}\nDECEASED cases : {deceased}\n")
 
 if __name__ == '__main__':
     # GET data from API:
@@ -41,18 +38,23 @@ if __name__ == '__main__':
 
     # Filter state data:
     states = [data1.index(x) for x in data1 if state in x['state'].lower()]
+
+    if len(states) == 0:
+        abort('No such state found!')
     
     for st in states:
         state_code = data1[st]['statecode']
         print(f"\n*** STATE NAME : {data1[st]['state']} ***")
-        state_data = data2[state_code]['total']
+        state_data = data2[state_code].get('total')
         
-        print_paramters(state_data,'state')
+        if state_data:
+            print_paramters(state_data)
+            districts_data = data1[st]['districtData']
+            districts = [districts_data.index(x) for x in districts_data if district in x['district'].lower()]
 
-        districts_data = data1[st]['districtData']
-        districts = [districts_data.index(x) for x in districts_data if district in x['district'].lower()]
-
-        for dist in districts:
-            district_data = districts_data[dist]
-            print(f"\n{districts.index(dist)+1}) DISTRICT NAME: {district_data['district']}")
-            print_paramters(district_data,'district')
+            for dist in districts:
+                district_data = districts_data[dist]
+                print(f"\n{districts.index(dist)+1}) DISTRICT NAME: {district_data['district']}")
+                print_paramters(district_data)
+        else:
+            pass
