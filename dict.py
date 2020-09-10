76c07b7b46aaf08f701b61f223bf04f0eed2d5ae
Suhas Kowligi <kowligi1998@gmail.com>
2020-09-10 19:23:39 +0530

    changed to lexico.com

--- dict.py
+++ dict.py
@@ -10,14 +10,11 @@ else:
     word = input("Enter the word\n")
 
 try:
-    r = requests.get("https://dictionary.cambridge.org/dictionary/english/{}".format(word), headers={'User-Agent': 'masterbyte'})
-    s = BeautifulSoup(r.text,'html.parser')
-    sections = s.findAll('div',attrs={'class':'sense-body'})
-    meaning = ''
-    for i in sections:
-        meaning += i.text
-    if len(meaning) == 0:
-        raise AttributeError
-    print("Word : '{}'\n\nMeaning : {}".format(word,meaning.lstrip()))
-except AttributeError:
+    r = requests.get("https://www.lexico.com/definition/{}".format(word), headers={'User-Agent': 'masterbyte'})
+    s = BeautifulSoup(r.text, 'html.parser')
+    
+    meanings = s.findAll('span',{'class':'ind'})
+    for index, meaning in enumerate(meanings, 1):
+        print(index, ' - ', meaning.text)
+except:
     print('No such word in my dictionary...')
