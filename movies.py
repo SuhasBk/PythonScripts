e6b7ce12dc07da4bbef7fef0f6a7a94a3d144836
Suhas Bk <kowligi1998@gmail.com>
2021-03-26 17:47:08 +0530

    play movies from local

---
+++ movies.py
@@ -0,0 +1,57 @@
+#!/usr/local/bin/python3
+from open_apps import start
+import os
+import sys
+
+if sys.platform == 'linux':
+    ROOT_DIR = '/mnt/Universe/tv_movies/movies'
+elif sys.platform == 'darwin':
+    ROOT_DIR = '/Users/gandalf/Movies'
+else:
+    ROOT_DIR = r'E:\tv_movies\movies'
+
+
+def file_filter(
+    x): return '.srt' not in x and '.txt' not in x and '.unwanted' not in x and '.parts' not in x
+
+
+series_list = list(map(lambda x: os.path.join(
+    ROOT_DIR, x), os.listdir(ROOT_DIR)))
+series_list = sorted(list(filter(lambda x: os.path.isdir(x), series_list)))
+
+for i, j in enumerate(series_list, 1):
+    print(i, ' : ', j[j.rfind(os.path.sep)+1:])
+
+ch = int(input("Enter the choice:\n> ")) - 1
+
+series = os.path.join(ROOT_DIR, series_list[ch])
+
+season_list = sorted(os.listdir(series))
+season_list = list(filter(file_filter, season_list))
+
+for p, q in enumerate(season_list, 1):
+    print(p, ' : ', q)
+
+ch = int(input("Enter the choice:\n> ")) - 1
+
+season = os.path.join(series, season_list[ch])
+
+if os.path.isdir(season):
+    episode_list = sorted(os.listdir(season))
+    episode_list = list(filter(file_filter, episode_list))
+
+    for z, w in enumerate(episode_list, 1):
+        print(z, ' : ', w)
+
+    ch = int(input("Enter the choice:\n> ")) - 1
+
+    episode = os.path.join(season, episode_list[ch])
+
+    if os.path.isdir(episode):
+        fname = list(filter(file_filter, sorted(os.listdir(episode))))
+        start("vlc", fname[0])
+    else:
+        start("vlc", episode)
+
+elif os.path.isfile(season):
+    start("vlc", season)
