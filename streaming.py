51149788f38179acf5b2da412d1b0d2c6edd0084
Suhas Bk <kowligi1998@gmail.com>
2021-03-27 10:18:39 +0530

    flexibility improvements

--- streaming.py
+++ streaming.py
@@ -13,16 +13,26 @@ service_mapping = {
     }
 }
 
-if len(sys.argv[1:]) > 0 and sys.argv[1] in service_mapping.keys():
-    service = sys.argv[1]
+if len(sys.argv[1:]) > 0 and sys.argv[1].lower() in (service_mapping.keys(), 'all'):
+    service = sys.argv[1].lower()
     artifact = '+'.join(sys.argv[2:]).lower()
 
-    base_url = service_mapping.get(service)['base_url']
-
-    if artifact != '':
-        search_url = service_mapping.get(service)['search_url'].replace('=',f'={artifact}')
-        webbrowser.open(search_url)
+    if service == 'all':
+        if artifact:
+            for streaming_service in service_mapping.keys():
+                search_url = service_mapping.get(streaming_service)['search_url'].replace('=',f'={artifact}')
+                webbrowser.open(search_url)
+        else:
+            for streaming_service in service_mapping.keys():
+                base_url = service_mapping.get(streaming_service)['base_url']
+                webbrowser.open(base_url)
     else:
-        webbrowser.open(base_url)
+        base_url = service_mapping.get(service)['base_url']
+
+        if artifact:
+            search_url = service_mapping.get(service)['search_url'].replace('=',f'={artifact}')
+            webbrowser.open(search_url)
+        else:
+            webbrowser.open(base_url)
 else:
-    exit("Syntax: python streaming.py {streaming_service ('prime', 'hotstar')} {artifact}")
+    exit("Syntax: python streaming.py {streaming_service ('prime', 'hotstar', 'all')} {artifact}")
