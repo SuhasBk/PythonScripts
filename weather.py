772ef414e4cb9b0c5d7a075163f8731b8787c299
Suhas Bk <kowligi1998@gmail.com>
2021-04-02 22:15:44 +0530

    formatting

--- weather.py
+++ weather.py
@@ -1,22 +1,27 @@
 #!/usr/local/bin/python3
-import requests
+import requests, sys
 from bs4 import BeautifulSoup
 
-country = input("Enter the country:\n> ")
-city = input(f"Enter any city in {country}:\n> ")
+try:
+    country = sys.argv[1]
+    city = sys.argv[2]
+except:
+    exit('usage: python weather.py [country] [city]')
 
 headers = {'User-Agent': 'masterbyte'}
 r = requests.get(f"https://www.timeanddate.com/weather/{country}/{city}",headers=headers)
 if r.ok:
     s = BeautifulSoup(r.text,'html.parser')
     a = s.find('div',attrs={'id':'qlook'})
+    res = ''
     try:
         for i in list(a)[1::]:
             if i.text.count('°C') > 1:
                 for j in i.text.split('°C'):
-                    print(j)
+                    res+=j+'\n'
             else:
-                print(i.text)
+                res+=i.text+'\n'
+        print(res.replace('\n', '. '))
     except:
         print("Bad city")
 else:
