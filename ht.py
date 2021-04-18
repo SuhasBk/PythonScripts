15a15a708bd9ce7e7fc1b09989a1ca388a26e0f3
Suhas Bk <kowligi1998@gmail.com>
2021-04-18 12:51:41 +0530

    download bangalore paper

---
+++ ht.py
@@ -0,0 +1,44 @@
+#!/usr/local/bin/python3
+
+# BIG GUN:
+
+# import os
+# import sys
+# import time
+# from msedge.selenium_tools import Edge, EdgeOptions
+
+# opt = EdgeOptions()
+# opt.use_chromium = True
+# del opt.capabilities['platform']
+# if len(sys.argv[1:]) == 0:
+#     opt.add_argument('--headless')
+
+# browser = Edge(options=opt, service_log_path=os.path.devnull)
+# browser.maximize_window()
+# browser.get('https://epaper.hindustantimes.com/Home/ArticleView')
+
+# browser.execute_script(f'DownloadAsEditionPdf("102","{date.day}/{str(date.month).zfill(2)}/{date.year}",5)')
+# time.sleep(5)
+# browser.quit()
+
+# much elegant:
+
+import requests
+import shutil
+from datetime import date
+
+date = date.today()
+
+session = requests.Session()
+
+file_name = session.get(f"https://epaper.hindustantimes.com/Home/downloadpdfedition_page?id=102&type=5&Date={date.day}%2F{str(date.month).zfill(2)}%2F{date.year}").json().get('FileName')
+
+file_download_url = f"https://epaper.hindustantimes.com/Home/Download?Filename={file_name}"
+
+file_name = str(date)+'.pdf'
+
+with session.get(file_download_url, stream=True) as r:
+    with open(str(date)+'.pdf', 'wb') as f:
+        shutil.copyfileobj(r.raw, f, length=16*1024*1024)
+
+session.close()
