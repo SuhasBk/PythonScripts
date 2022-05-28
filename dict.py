6145af8deabd784bfd832962ac07aa758fac9ce1
Suhas Bk <kowligi1998@gmail.com>
2022-05-28 21:43:50 +0530

    changes

--- dict.py
+++ dict.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 import requests
 from bs4 import BeautifulSoup
-from fake_useragent import  UserAgent
 import sys
+
+word = None
 
 if len(sys.argv) > 1:
     word = sys.argv[1]
-else:
-    word = input("Enter the word\n")
 
-try:
-    r = requests.get("https://www.lexico.com/definition/{}".format(word), headers={'User-Agent': 'masterbyte'})
-    s = BeautifulSoup(r.text, 'html.parser')
-    
-    meanings = s.findAll('span',{'class':'ind'})
-    for index, meaning in enumerate(meanings, 1):
-        print(index, ' - ', meaning.text)
-except:
-    print('No such word in my dictionary...')
+while True:
+    if not word:
+        word = input("\n\nEnter the word\n")
+
+    try:
+        r = requests.get("https://www.lexico.com/definition/{}".format(word), headers={'User-Agent': 'masterbyte'})
+        s = BeautifulSoup(r.text, 'html.parser')
+        
+        meanings = s.findAll('span',{'class':'ind'})
+        for index, meaning in enumerate(meanings, 1):
+            print(index, ' - ', meaning.text)
+    except:
+        print('No such word in my dictionary...')
+    word = None
