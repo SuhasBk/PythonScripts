5a54b63cb955fd71d0c121b1cf845ef99eb585c4
Suhas Bk <kowligi1998@gmail.com>
2023-12-06 11:13:44 -0600

    handle ms news rewards

--- rewards.py
+++ rewards.py
@@ -58,7 +58,7 @@ def mobile_new():
                 run(['adb', 'shell', 'input', 'text', item])
                 # press enter
                 run(['adb', 'shell', 'input', 'keyevent', '66'])
-                time.sleep(random.randint(3, 7))
+                time.sleep(random.randint(5, 10))
             i += 1
         s.add(item)
 
@@ -75,7 +75,7 @@ def desktop_new():
                 gui.write(item)
                 time.sleep(1)
                 gui.press('enter')
-            time.sleep(random.randint(3, 7))
+            time.sleep(random.randint(5, 10))
             i += 1
         s.add(item)
     
@@ -84,23 +84,51 @@ def desktop_new():
         gui.hotkey('command', 'w')
         time.sleep(0.5)
 
+def news():
+    run(['adb', 'shell', 'am', 'start', '-n', BING_ACTIVITY])
+    time.sleep(9)
+
+    run(['adb', 'shell', 'input', 'tap', '100', '2000'])
+
+    time.sleep(4)
+    i = 0
+    while i<5:
+        run(['adb', 'shell', 'input', 'swipe', '100', '500', '100', '1450', '100'])
+        time.sleep(3)
+        run(['adb', 'shell', 'input', 'tap', '500', '500'])
+        time.sleep(3)
+        run(['adb', 'shell', 'input', 'keyevent', '4'])
+        time.sleep(0.5)
+        run(['adb', 'shell', 'input', 'tap', '500', '900'])
+        time.sleep(3)
+        run(['adb', 'shell', 'input', 'keyevent', '4'])
+        time.sleep(0.5)
+        run(['adb', 'shell', 'input', 'tap', '500', '1300'])
+        time.sleep(3)
+        run(['adb', 'shell', 'input', 'keyevent', '4'])
+        i += 1
+
 if __name__ == '__main__':
     EDGE_ACTIVITY = "com.microsoft.emmx/com.microsoft.ruby.Main"
+    BING_ACTIVITY = "com.microsoft.bing/com.microsoft.sapphire.app.main.SapphireMainActivity"
     s = set()
 
     try:
+        webbrowser.open("https://rewards.bing.com/pointsbreakdown")
         r = requests.get('https://random-word-api.herokuapp.com/word?number=100')
         items = r.json()
     except:
         items = random.sample(words.words(), 100)    
 
     try:
-        webbrowser.open("https://rewards.bing.com/pointsbreakdown")
         arg1 = sys.argv[1]
         if arg1 == 'mobile':
             mobile_new()
+        elif arg1 == 'news':
+            news()
         else:
             desktop_new()
     except IndexError:
         mobile_new()
         desktop_new()
+        news()
